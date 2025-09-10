30bf0c62625e34de67f630fb0fc997e4b0705595
cjea <cj@speakeasy.com>
2025-09-10 15:24:19 -0700

    test that api key is loaded

--- server/cli/main.go
+++ server/cli/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
-var audience = "world"
-
 func main() {
-	fmt.Printf("Hello, %s!", audience)
+	key := apiKeyFromEnv()
+	ok := strings.HasPrefix(key, "gram")
+
+	fmt.Printf("API key looks good: %v", ok)
+}
+
+func apiKeyFromEnv() string {
+	return mustEnv("GRAM_API_KEY")
+}
+
+func mustEnv(key string) string {
+	val := os.Getenv(key)
+	if len(val) == 0 {
+		panic(fmt.Errorf("missing env: %s", key))
+	}
+
+	return val
 }
